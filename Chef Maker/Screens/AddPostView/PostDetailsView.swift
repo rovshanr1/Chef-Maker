0e19b949a2b21a72f25b3e852bb08d33affae601
Rovshan Rasulov <soulofchinder.2@gmail.com>
2025-05-27 21:16:28 +0400

    fix: Moved the Post TitleInsideViewPostHandle function to PostViewModel and added all state methods to published in viewmodel

---
+++ Chef Maker/Screens/AddPostView/PostDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  PostDetailsView.swift
+//  Chef Maker
+//
+//  Created by Rovshan Rasulov on 27.05.25.
+//
+
+import SwiftUI
+
+struct PostDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostDetailsView()
+}
